2ba62ea4664bb6ccc0c18f8282b1afc17fd3b161
Adriano Saadeh <adrianosaadeh@gmail.com>
2018-11-04 17:12:34 -0200

    CRUD Candidate refatorado e incluída a dependência modelmapper

--- candidate-master/src/main/java/br/edu/ulbra/election/candidate/service/CandidateService.java
+++ candidate-master/src/main/java/br/edu/ulbra/election/candidate/service/CandidateService.java
@@ -1,117 +1,28 @@
 package br.edu.ulbra.election.candidate.service;
 
-import br.edu.ulbra.election.candidate.exception.GenericOutputException;
-import br.edu.ulbra.election.candidate.input.v1.CandidateInput;
-import br.edu.ulbra.election.candidate.model.Candidate;
-import br.edu.ulbra.election.candidate.output.v1.GenericOutput;
-import br.edu.ulbra.election.candidate.output.v1.CandidateOutput;
-import br.edu.ulbra.election.candidate.repository.CandidateRepository;
-import org.apache.commons.lang.StringUtils;
+import java.lang.reflect.Type;
+import java.util.List;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.lang.reflect.Type;
-import java.util.List;
+import br.edu.ulbra.election.candidate.output.v1.CandidateOutput;
+import br.edu.ulbra.election.candidate.repository.CandidateRepository;
 
 @Service
 public class CandidateService {
 
     private final CandidateRepository candidateRepository;
-
     private final ModelMapper modelMapper;
 
-    private final PasswordEncoder passwordEncoder;
-
-    private static final String MESSAGE_INVALID_ID = "Invalid id";
-    private static final String MESSAGE_VOTER_NOT_FOUND = "Candidate not found";
-
     @Autowired
-    public CandidateService(CandidateRepository candidateRepository, ModelMapper modelMapper, PasswordEncoder passwordEncoder){
+    public CandidateService(CandidateRepository candidateRepository, ModelMapper modelMapper){
         this.candidateRepository = candidateRepository;
         this.modelMapper = modelMapper;
-        this.passwordEncoder = passwordEncoder;
     }
 
     public List<CandidateOutput> getAll(){
         Type candidateOutputListType = new TypeToken<List<CandidateOutput>>(){}.getType();
         return modelMapper.map(candidateRepository.findAll(), candidateOutputListType);
     }
-
-    public CandidateOutput create(CandidateInput candidateInput) {
-        validateInput(candidateInput, false);
-        Candidate candidate = modelMapper.map(candidateInput, Candidate.class);
-        candidate.setPassword(passwordEncoder.encode(candidate.getPassword()));
-        candidate = candidateRepository.save(candidate);
-        return modelMapper.map(candidate, CandidateOutput.class);
-    }
-
-    public CandidateOutput getById(Long candidateId){
-        if (candidateId == null){
-            throw new GenericOutputException(MESSAGE_INVALID_ID);
-        }
-
-        Candidate candidate = candidateRepository.findById(candidateId).orElse(null);
-        if (candidate == null){
-            throw new GenericOutputException(MESSAGE_VOTER_NOT_FOUND);
-        }
-
-        return modelMapper.map(candidate, CandidateOutput.class);
-    }
-
-    public CandidateOutput update(Long candidateId, CandidateInput candidateInput) {
-        if (candidateId == null){
-            throw new GenericOutputException(MESSAGE_INVALID_ID);
-        }
-        validateInput(candidateInput, true);
-
-        Candidate candidate = candidateRepository.findById(candidateId).orElse(null);
-        if (candidate == null){
-            throw new GenericOutputException(MESSAGE_VOTER_NOT_FOUND);
-        }
-
-        candidate.setEmail(candidateInput.getEmail());
-        candidate.setName(candidateInput.getName());
-        if (!StringUtils.isBlank(candidateInput.getPassword())) {
-        	candidate.setPassword(passwordEncoder.encode(candidateInput.getPassword()));
-        }
-        candidate = candidateRepository.save(candidate);
-        return modelMapper.map(candidate, CandidateOutput.class);
-    }
-
-    public GenericOutput delete(Long candidateId) {
-        if (candidateId == null){
-            throw new GenericOutputException(MESSAGE_INVALID_ID);
-        }
-
-        Candidate candidate = candidateRepository.findById(candidateId).orElse(null);
-        if (candidate == null){
-            throw new GenericOutputException(MESSAGE_VOTER_NOT_FOUND);
-        }
-
-        candidateRepository.delete(candidate);
-
-        return new GenericOutput("Candidate deleted");
-    }
-
-    private void validateInput(CandidateInput candidateInput, boolean isUpdate){
-        if (StringUtils.isBlank(candidateInput.getEmail())){
-            throw new GenericOutputException("Invalid email");
-        }
-        if (StringUtils.isBlank(candidateInput.getName())){
-            throw new GenericOutputException("Invalid name");
-        }
-        if (!StringUtils.isBlank(candidateInput.getPassword())){
-            if (!candidateInput.getPassword().equals(candidateInput.getPasswordConfirm())){
-                throw new GenericOutputException("Passwords doesn't match");
-            }
-        } else {
-            if (!isUpdate) {
-                throw new GenericOutputException("Password doesn't match");
-            }
-        }
-    }
-
 }
